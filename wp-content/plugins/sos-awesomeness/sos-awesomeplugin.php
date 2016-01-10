455fed6ef44f4ee11a7f50bfd23f29f0ff19968e
Chris Hattery <cmhatte@yahoo.com>
2016-01-10 16:44:29 -0500

    base plugin

---
+++ wp-content/plugins/sos-awesomeness/sos-awesomeplugin.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name: Awesomeness Creator
+ * Plugin URI:
+ * Description: a plugin to create awesomeness and spread joy
+ * Version 0.0.1
+ * Author:
+ * Author URI:
+ * License: GPL2
+ * Tutorial URL: https://www.smashingmagazine.com/2011/09/how-to-create-a-wordpress-plugin/
+ */
+
